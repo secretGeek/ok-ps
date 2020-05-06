40c2218ebcb0428b0176bca6787a1bb3c16b30bf
secretGeek <leonbambrick@gmail.com>
2020-05-06 23:45:40 +1000

    Some examples

---
+++ Examples/Intermediate Examples/build.ps1
@@ -0,0 +1,26 @@
+# Call this with "ok build"
+$messages = 
+	"Building code...",
+	"Reticulating splines....",
+	"Calculating vectors...",
+	"Deleting files...",
+	"Ooops....",
+	"Restoring files....",
+	"Trying to look busy....",
+	"Have you ever noticed...",
+	"The way people will patiently wait...",
+	"For a script to finish...",
+	"Never sure if they should just press Ctrl-C...",
+	"Or if it will go on for ever...",
+	"Building code...",
+	"Reticulating splines....",
+	
+
+$messages | Foreach-Object {
+	Write-host $_ 
+	Start-Sleep 1
+}
+
+ok build; # Yeh it calls itself and does go on forever. Use Ctrl C to exit.
+
+
